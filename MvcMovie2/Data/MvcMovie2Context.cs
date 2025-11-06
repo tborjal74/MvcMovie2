79d5f485f83a47c26342e1c2cad18eb5f8cf5c2d
tborjal74 <tborjal@byupathway.edu>
2025-11-06 20:11:49 +0800

    updated CRUD

---
+++ MvcMovie2/Data/MvcMovie2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie2.Data
+{
+    public class MvcMovie2Context : DbContext
+    {
+        public MvcMovie2Context (DbContextOptions<MvcMovie2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
