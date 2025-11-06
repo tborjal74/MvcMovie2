ba14ad596fe4898cc656fac11730bb21ddd67cbc
tborjal74 <tborjal@byupathway.edu>
2025-11-06 21:46:15 +0800

    added search

---
+++ MvcMovie2/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MvcMovie2.Models;
+
+namespace MvcMovie2.Models;
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+}
